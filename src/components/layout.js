1f8cbb0efe0810cbe65d70c9582929aeee43d218
Adam Goth <adamgoth@gmail.com>
2019-03-06 18:49:09 -0700

    More clean up, add base styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,7 @@
 
 import React from "react"
 import Header from "./header"
+import Footer from "./footer"
 
 import "./layout.css"
 
@@ -14,7 +15,7 @@ const Layout = ({ children }) => (
   <React.Fragment>
     <Header />
     <main>{children}</main>
-    <footer>footer</footer>
+    <Footer />
   </React.Fragment>
 )
 
